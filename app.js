f26cc39d296e2a2863b0342fe82b9bac00496f0b
Aishwarya Pathak <pathakaishwarya54@gmail.com>
2022-08-30 01:10:33 +0530

    Image resources changed

--- app.js
+++ app.js
@@ -20,4 +20,36 @@ function selectChoice(){
     selectSound.play();
     you=this.id;
     document.getElementById('your-choice').src ='src/'+ you +'.png';
-};
+
+    //Random for opponent
+    opponent=choices[Math.floor(Math.random()*3)]; //0-0.9999*3=0-2.9999
+    document.getElementById('opponent-choice').src='src/'+ opponent +'.png';
+
+    //Check for winner
+    if(you==opponent){
+        yourScore+=1;
+        opponentScore+=1;
+    }
+    else{
+        if(you=='rock' && opponent=='paper'){
+            opponentScore+=1;
+        }
+        else if(you=='paper' && opponent=='scissor'){
+            opponentScore+=1;
+        }
+        else if(you=='scissor' && opponent=='rock'){
+            opponentScore+=1;
+        }
+        else if(you=='paper' && opponent=='rock'){
+            yourScore+=1;
+        }
+        else if(you=='scissor' && opponent=='paper'){
+            yourScore+=1;
+        }
+        else if (you=='rock' && opponent=='scissor'){
+            yourScore+=1;
+        }
+    }
+    document.getElementById('your-score').innerText=yourScore;
+    document.getElementById('opponent-score').innerText=opponentScore;
+}
